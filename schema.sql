71d0b9b7cdcb8954ec097f2b36586cf0ab9ccc78
kumar56satyam <kumar56satyam@gmail.com>
2026-07-27 09:54:00 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -25,3 +25,30 @@ CREATE TABLE categories (
     description TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+
+
+
+
+CREATE TABLE products (
+    product_id BIGSERIAL PRIMARY KEY,
+    category_id BIGINT NOT NULL,
+    product_name VARCHAR(200) NOT NULL,
+    brand VARCHAR(100) NOT NULL,
+    sku VARCHAR(50) UNIQUE NOT NULL,
+    unit_price NUMERIC(10,2) NOT NULL CHECK (unit_price > 0),
+    cost_price NUMERIC(10,2) NOT NULL CHECK (cost_price > 0),
+    stock_quantity INT NOT NULL DEFAULT 0 CHECK (stock_quantity >= 0),
+    weight_kg NUMERIC(6,2),
+    color VARCHAR(50),
+    launch_date DATE,
+    product_status VARCHAR(20) DEFAULT 'Active'
+        CHECK (product_status IN ('Active','Discontinued','Out of Stock')),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_products_category
+        FOREIGN KEY (category_id)
+        REFERENCES categories(category_id)
+);
+
