6ea978c49d868defacce5335dc9faa207e7dff03
kumar56satyam <kumar56satyam@gmail.com>
2026-07-27 09:52:04 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,16 @@
 
+
+
+CREATE TABLE customers (
+    customer_id BIGSERIAL PRIMARY KEY,
+    customer_name VARCHAR(150) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    phone VARCHAR(15) UNIQUE,
+    gender VARCHAR(10),
+    date_of_birth DATE,
+    city VARCHAR(100),
+    state VARCHAR(100),
+    signup_date DATE NOT NULL,
+    customer_status VARCHAR(20) DEFAULT 'Active'
+);
+
